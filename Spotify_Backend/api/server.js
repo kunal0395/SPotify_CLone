55f7eb23f7ffd9f2f1026329832fe201930a1fec
Kunal Patil <kp3244510@gmail.com>
2024-09-06 21:35:13 +0530

    adding api

--- Spotify_Backend/api/server.js
+++ Spotify_Backend/api/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import songRouter from "./src/routes/songRoute.js";
-import albumRouter from "./src/routes/albumRoute.js";
-import connectDB from "./src/config/mongodb.js";
-import connectCloudinary from "./src/config/cloudinary.js";
+import songRouter from "../src/routes/songRoute.js";
+import albumRouter from "../src/routes/albumRoute.js";
+import connectDB from "../src/config/mongodb.js";
+import connectCloudinary from "../src/config/cloudinary.js";
 
 // App config
 const app = express();
